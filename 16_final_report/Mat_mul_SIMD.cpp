76aee3a00b391d29aff1cd029d40e8f12f1e9c9f
ReturnHao <fluctuation.h@gmail.com>
2021-05-27 18:15:58 +0800

    Updated@Final Report

---
+++ 16_final_report/Mat_mul_SIMD.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <immintrin.h>
+#include <omp.h>
+
+#define N 512
+
+float *matrix_a;
+float *matrix_b;
+float result[N][N];
+
+void chunked_mm(int chunk, int n_chunks) {
+    __m256 va, vb, vc;
+    for (int i = chunk*(N/n_chunks); i < (chunk+1)*(N/n_chunks); i++) {
+        for (int j = 0; j < N; j++) {
+            float buffer[8] = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+            vc = _mm256_loadu_ps(buffer);
+            for (int k = 0; k < N; k += 8) {
+                // load
+                va = _mm256_loadu_ps(matrix_a+(i*N)+k); // matrix_a[i][k]
+                vb = _mm256_loadu_ps(matrix_b+(j*N)+k); // matrix_b[j][k]
+
+                // fused multiply and add
+                vc = _mm256_fmadd_ps(va, vb, vc);
+            }
+            //vc = _mm256_hadd_ps(vc, vc);
+            _mm256_storeu_ps(buffer, vc);
+            result[i][j] = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5] + buffer[6] + buffer[7];
+            //result[i][j] = buffer[0] + buffer[2] + buffer[4] + buffer[6];
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    // initialize matrix_a and matrix_b
+    matrix_a = malloc(N*N*sizeof(float));
+    matrix_b = malloc(N*N*sizeof(float));
+
+    for (int i = 0; i < N*N; i++) {
+        *(matrix_a+i) = 0.1f;
+        *(matrix_b+i) = 0.2f;
+    }
+    // initialize result matrix
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            result[i][j] = 0.0f;
+        }
+    }
+
+    #pragma omp parallel for
+    for (int i = 0; i < 4; i++) {
+        chunked_mm(i, 4);
+    }
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            //printf("%f ", result[i][j]);
+            printf("%x ", *(unsigned int*)&result[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
