ae93945de95e856f882d393b185c439d4922117b
ReturnHao <fluctuation.h@gmail.com>
2021-05-31 03:36:24 +0800

    Updated@Final Report

--- 16_final_report/MPI_CUDA/Matrix_mult_MPI_Cuda.cu
+++ 16_final_report/MPI_CUDA/Matrix_mult_MPI_Cuda.cu
@@ -5,11 +5,11 @@
 //  Author: Fang Hao
 //
 //  Compile with:
-//  nvcc Matrix_mult_MPI_Cuda.cu -lmpi
+//  nvcc Matrix_mult_MPI_Cuda.cu -lmpi -std=c++11
+//  mpirun -np 4 ./a.out
 //
 #include <mpi.h>
 #include <bits/stdc++.h>
-#include <omp.h>
 using namespace std;
 #define BLOCK_SIZE 16
 
@@ -17,10 +17,12 @@ __global__ void GPU_matrix_mult(float *a, float *b, float *c, int N, int size, i
 {
     int i = blockIdx.y * BLOCK_SIZE + threadIdx.y;
     int j = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    float sum = 0.0f;
     if ((i < N / size) && (j < N / size))
     {
         for (int k = 0; k < N; k++)
-            c[N * i + j + Offset] += a[N * i + k] * b[N / size * k + j];
+            sum += a[N * i + k] * b[N / size * k + j];
+        c[N * i + j + Offset] = sum;
     }
 }
 
@@ -31,24 +33,29 @@ int main(int argc, char** argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    // Assign diveces to the MPI process
+    int gpusize, gpurank;
+    cudaGetDeviceCount(&gpusize);
+    cudaSetDevice(rank % gpusize);
+    cudaGetDevice(&gpurank);
 
     // Generate Matrix
     const int N = 256;
-    float *A, *B, *C, *subA, *subB, *subC, *recv;
-    cudaMallocHost(&A, N * N * sizeof(float));
-    cudaMallocHost(&B, N * N * sizeof(float));
-    cudaMallocHost(&C, N * N * sizeof(float));
-    cudaMallocHost(&subA, N * N / size * sizeof(float));
-    cudaMallocHost(&subB, N * N / size * sizeof(float));
-    cudaMallocHost(&subC, N * N / size * sizeof(float));
-    cudaMallocHost(&recv, N * N / size * sizeof(float));
+    vector<float> A(N * N);
+    vector<float> B(N * N);
+    vector<float> C(N * N, 0);
+    float *subA, *subB, *subC, *recv;
+    subA = new float [N * N / size];
+    subB = new float [N * N / size];
+    subC = new float [N * N / size];
+    recv = new float [N * N / size];
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             A[N * i + j] = drand48();
             B[N * i + j] = drand48();
-            C[N * i + j] = 0.0f;
         }
     }
     
@@ -65,12 +72,13 @@ int main(int argc, char** argv)
     int send_to = (rank - 1 + size) % size;
     
     // Initialize CUDA grid and block
-    cudaMalloc(&d_a, N * N / size * sizeof(float));
-    cudaMalloc(&d_b, N * N / size * sizeof(float));
-    cudaMalloc(&d_c, N * N / size * sizeof(float));
+    float *d_a, *d_b, *d_c;
+    cudaMalloc((void **) &d_a, N * N / size * sizeof(float));
+    cudaMalloc((void **) &d_b, N * N / size * sizeof(float));
+    cudaMalloc((void **) &d_c, N * N / size * sizeof(float));
     
-    cudaMemcpy(d_a, subA, N * N / size * sizeof(float));
-    cudaMemcpy(d_b, subB, N * N / size * sizeof(float));
+    cudaMemcpy(d_a, subA, N * N / size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, subB, N * N / size * sizeof(float), cudaMemcpyHostToDevice);
     unsigned int grid_n = ((N / size) + BLOCK_SIZE - 1) / BLOCK_SIZE;
     
     dim3 dimGrid(grid_n, grid_n);
@@ -78,6 +86,7 @@ int main(int argc, char** argv)
 
     // Parallel Matrix Multiplication
     double comp_time = 0, comm_time = 0;
+    
     for (int irank = 0; irank < size; irank++)
     {
         // Record the time-stamp before sub parallel process start
@@ -85,8 +94,8 @@ int main(int argc, char** argv)
         
         offset = N / size * ((rank + irank) % size);
         GPU_matrix_mult<<<dimGrid, dimBlock >>>(d_a, d_b, d_c, N, size, offset);
-        cudaMemcpy(subC, d_c, N * N / size, cudaMemcpyDeviceToHost);
         cudaDeviceSynchronize();
+        cudaMemcpy(subC, d_c, N * N / size * sizeof(float), cudaMemcpyDeviceToHost);
         // Record the time-stamp after sub calculation process end
         auto toc = chrono::steady_clock::now();
         comp_time += chrono::duration<double>(toc - tic).count();
@@ -97,14 +106,15 @@ int main(int argc, char** argv)
         MPI_Irecv(&recv[0], N * N / size, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &request[1]);
         MPI_Waitall(2, request, MPI_STATUS_IGNORE);
         for (int i = 0; i < N * N / size; i++) subB[i] = recv[i];
-        
+        cudaMemcpy(d_b, subB, N * N / size * sizeof(float), cudaMemcpyHostToDevice);
+
         // Record the time-stamp after whole process end
         tic = chrono::steady_clock::now();
         comm_time += chrono::duration<double>(tic - toc).count();
     }
     // Allgather result data
     MPI_Allgather(&subC[0], N * N / size, MPI_FLOAT, &C[0], N * N / size, MPI_FLOAT, MPI_COMM_WORLD);
-  
+    
     // Error check
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
@@ -130,3 +140,4 @@ int main(int argc, char** argv)
     // Finalization
     MPI_Finalize();
 }
+
